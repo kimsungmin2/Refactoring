113ddcfb37146a67ba278d070246f1c3928d238d
kimsungmin2 <154573616+kimsungmin2@users.noreply.github.com>
2024-02-01 18:02:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import LogMiddleware from "./middlewares/log.middleware.js";
 import ErrorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 import UsersRouter from "./routes/users.router.js";
 import ResumeRouter from "./routes/resumes.router.js";
-import cors from "cors";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
@@ -34,12 +33,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", [UsersRouter, ResumeRouter]);
 app.use(ErrorHandlingMiddleware);
-app.use(
-  cors({
-    origin: "http://localhost:3020",
-    credentials: true,
-  })
-);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
